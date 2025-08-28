64047d8fc0974d8a6673401f3591b4ebb8b8e943
armaan535kh <kh.armaan535@gmail.com>
2025-08-28 12:09:23 +0530

    [Feature] Created Display for the calculator.

--- Calculator/CalculatorView.swift
+++ Calculator/CalculatorView.swift
@@ -10,11 +10,21 @@ import SwiftUI
 struct CalculatorView: View {
     var body: some View {
         VStack {
-            
+            Display
         }
     }
 }
 
 #Preview {
     CalculatorView()
+}
+
+extension CalculatorView {
+    
+    private var Display : some View{
+        Text("0")
+            .font(.largeTitle)
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
+            .padding(EdgeInsets(top: 100, leading: 0, bottom: 0, trailing: 25))
+    }
 }
