d511624ca0e4f2722af1afbd4a50a6c179467266
armaan535kh <kh.armaan535@gmail.com>
2025-08-28 07:25:23 +0530

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Armaan Khan  on 28/08/25.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
